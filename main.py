e7a7d74430ca8a46ce1c8b256d1697e3880265b3
Anurag Singh <singhanurag024@gmail.com>
2022-03-25 03:09:35 +0530

    chore: Added assets for page cover

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import json
 from json import JSONEncoder
 
 secret_key = NOTION_SECRET_KEY
-habit_database_id = NOTION_HABIT_DB
+habit_db_id = NOTION_HABIT_DB
+analytics_db_id = NOTION_ANALYTICS_DB
 
 base_db_url = "https://api.notion.com/v1/databases/"
 base_pg_url = "https://api.notion.com/v1/pages/"
@@ -14,8 +15,13 @@ base_pg_url = "https://api.notion.com/v1/pages/"
 header = {"Authorization": secret_key,
           "Notion-Version": "2021-05-13", "Content-Type": "application/json"}
 
-response_stocks_db = requests.post(
-    base_db_url + habit_database_id + "/query", headers=header)
+response_habits_db = requests.post(
+    base_db_url + habit_db_id + "/query", headers=header)
+
+response_analytics_db = requests.post(base_db_url + analytics_db_id + "/query", headers=header)
+
+# define no. of new pages/records to be added in Tracker
+days_count = 15
 
 # subclass JSONEncoder
 class DateTimeEncoder(JSONEncoder):
@@ -25,8 +31,10 @@ class DateTimeEncoder(JSONEncoder):
                 return obj.isoformat()
 
 date_list = list()
+month_dict = dict()
 
-for page in response_stocks_db.json()['results']:
+# Creating list of datetime.date objects for current month pages/records
+for page in response_habits_db.json()['results']:
   page_id = page['id']
   props = page['properties']
   current_month = props['Date']['date']['start']
@@ -35,14 +43,22 @@ for page in response_stocks_db.json()['results']:
   if date_object.month == date.today().month:
     date_list.append(date_object)
 
-start_date = max(date_list) + timedelta(days=1)
+if not date_list:
+    start_date = date.today()
+else:
+    start_date = max(date_list) + timedelta(days=1)
 
-# define no. of new pages/records to be added in Tracker
-days_count = 30
+# Creating dict of page name and id for monthly analytics db
+for page in response_analytics_db.json()['results']:
+    page_id = page['id']
+    props = page['properties']
+    page_name = props['Name']['title'][0]['text']['content']
+    month_dict[page_name] = page_id
 
 for date in (start_date + timedelta(n) for n in range(days_count)):
-  # To fetch weekday from date
   day = date.strftime('%A')
+  month = date.strftime('%B')
+  relation_id = month_dict[month]
 
   # To make datetime.date object JSON serializable
   date_json = json.dumps(date, indent=4, cls=DateTimeEncoder)
@@ -62,6 +78,13 @@ for date in (start_date + timedelta(n) for n in range(days_count)):
                               "text": {
                                   "content": day
                               }
+                          }
+                      ]
+                  },
+                  "Relation": {
+                      "relation": [
+                          {
+                              "id": relation_id
                           }
                       ]
                   }
